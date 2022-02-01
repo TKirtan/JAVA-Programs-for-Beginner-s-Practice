df784a719b8e9b607388bb6fe20ed6226db6f2f5
Kirtan Kushwah <38884247+TKirtan@users.noreply.github.com>
2022-02-01 19:07:24 +0530

    Questions added

---
+++ Beginner Level Programs/7. Input any alphabet and check wheather it is vowel or consonant/Alphabet.java
@@ -0,0 +1,11 @@
+import java.lang.*;
+import java.util.*;
+
+class Alphabet {
+    public static void main(Strings[] args) {
+
+        Scanner s = new Scanner(System.in);
+
+        char a = s.nextchar();
+    }
+}
